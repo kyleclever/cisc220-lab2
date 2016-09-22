00c73aebf5fb9baa317611555dd5b591ab47fb4f
kylew <kylew@128.4.111.84>
2016-09-21 20:34:04 -0400

    initial

---
+++ lottery/LAB2LOTTERY.hpp
@@ -0,0 +1,44 @@
+/*
+ * LAB2LOTTERY.hpp
+ *
+ *  Created on: Sep 21, 2016
+ *      Author: kylew
+ */
+
+#ifndef LAB2LOTTERY_HPP_
+#define LAB2LOTTERY_HPP_
+
+
+	struct Customer {
+	int customerID;
+	int numOfTickets;
+	int *lotterynums;
+	};
+
+	struct Store{
+	int storeID;
+	int numOfCustomers;
+	int *customerList;
+	int numOfSold;
+	int numOfWinners[3]; // number of winners who match 1, 2 or 3 numbers
+	};
+
+	struct Owner{
+	int *stores;
+	int numOfStores;
+	int totalSold;
+	int totalWinners[3];
+	};
+
+
+	Owner *makeOwner();
+	Store makeStore();
+	Customer makeCustomer();
+	void getWinners (int winners[]);
+	void findWinners(Owner *owner, int *ls);
+	int checkwin(int *nums, int *winners);
+
+//lottery header
+
+
+#endif /* LAB2LOTTERY_HPP_ */
---
+++ lottery/lottery.cpp
@@ -0,0 +1,30 @@
+/*
+ * lottery.cpp
+ *
+ *  Created on: Sep 21, 2016
+ *      Author: kylew
+ */
+#include "LAB2LOTTERY.hpp"
+
+
+
+
+Owner *makeOwner(){
+	Owner ;
+	return Owner;
+}
+
+Store makeStore(){
+	return;
+
+}
+
+
+Customer makeCustomer(){
+	return;
+}
+
+int a(){
+	int b;
+	return b;
+}
