f1500bddef3fe29ea586927e5be36e551ed7f8a8
kylew <kylew@128.4.192.64>
2016-09-27 15:00:24 -0400

    update

--- lottery/lottery.cpp
+++ lottery/lottery.cpp
@@ -5,24 +5,41 @@
  *      Author: kylew
  */
 #include "LAB2LOTTERY.hpp"
+#include <stdlib.h>
+using namespace std;
 
-Customer makeCustomer(){
+Owner *makeOwner(){
+	int a = rand() % 10 +1;
+    Store x[];
 
-	return;
+    Owner totalsold = 0;
+    for (int i = 0; i < Owner (*x)[makeStore()]; i ++){
+           x[i] = makeStore();
+           Store Numsold;
+           totalsold += a * x[Numsold];
+    }//for
+    return x;
 }
 
+Store makeStore(){
+	int Storeid = rand() % 100 + 1;
+	int Customernum = rand() % 10 +1;
+
+	Customer x[];
+	Store Numsold;
+
+	for(int i = 0; i < Store (*x)[makeCustomer()];i++){
+		x[i] = makeCustomer();
+	    Customer numtickets;
+	    Numsold += Storeid * x[numtickets];
+	}//for
+	return Numsold;
+}// makeStore
 
 
-//Owner *makeOwner(){
-//	Owner ;
-//	return Owner;
-//}
-
-
+//Customer makeCustomer(){
 //
-//Store makeStore(){
 //	return;
-//
 //}
 
 
